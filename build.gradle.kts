e6441cac23e26c6b51090139ac43d54f3f2f68ec
Robin Andre <fi7528@kit.edu>
2026-02-13 14:19:55 +0100

    the main repo should have the gitlab ci stuff, not the example

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.21"
+    id("maven-publish")
 }
 
 group = "edu.kit.ifv.populationsynthesis"
@@ -8,6 +9,10 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://packages.jetbrains.team/maven/p/kds/kotlin-ds-maven")
+
+    maven { url = uri("https://nexus.ifv.kit.edu/repository/maven-releases/") }
+    maven { url = uri("https://nexus.ifv.kit.edu/repository/maven-central/") }
+    maven { url = uri("https://nexus.ifv.kit.edu/repository/maven-snapshots/") }
 }
 
 dependencies {
@@ -47,3 +52,89 @@ kotlin {
         freeCompilerArgs.add("-Xcontext-parameters")
     }
 }
+
+
+allprojects {
+    apply(plugin = "maven-publish")
+
+    project.group = "edu.kit.ifv.mobitopp"
+
+    afterEvaluate {
+
+        if (checkProperty("doPublish")) {
+            /* mobiTopp publishing process (see .gitlab-ci.yml)
+             * Parameters such as "doPublish" must be passed in gradle command:
+             *  - ./gradlew <TASKS> publish -PdoPublish=true -Pparam=value...
+             * Lookup of parameters doPublish and isRelease returns true if they are specified and their value reads "true".
+             * Other required parameters must be specified, otherwise an error is thrown.
+             *
+             * The pipeline build version is used as the published artifacts version string.
+             *  - uses parameter: "buildVersion"
+             *
+             * Every merge on main is published to local repo: see deploy-job
+             *  - checks: doPublish=true, isRelease=false
+             *  - requires parameters: "localUrl", "localRepoUser" and "localRepoPassword"
+             *
+             * Public releases must be published manually:
+             *  - checks: doPublish=true, isRelease=true
+             *  - requires parameters: "publicUrl", "publicRepoUser" and "publicRepoPassword"
+             */
+
+            project.version = requireProperty("buildVersion")
+            println("Setup publishing configuration for ${group}:${project.name}:${version}.")
+
+            publishing {
+
+                publications {
+                    register("mavenData", MavenPublication::class) {
+                        from(components["kotlin"]) // For Kotlin projects
+                        groupId = group.toString()
+                        artifactId = project.name
+                        version = project.version.toString()
+                    }
+                }
+
+                repositories {
+                    if (checkProperty("isRelease")) {
+                        println("Activate: publish public release!")
+                        println("WARNING: Public release still deactivated!")
+
+                        //  Keep for first public release of reengineered mobitopp
+                        //maven {
+                        //    name = "PublicRepo"
+                        //    url = uri(requireProperty("publicUrl"))
+                        //    credentials {
+                        //        username = requireProperty("publicRepoUser")
+                        //        password = requireProperty("publicRepoPassword")
+                        //    }
+                        //}
+
+                    } else {
+                        println("Activate: publish local build!")
+                        maven {
+                            name = "LocalRepo"
+                            url = uri(requireProperty("localUrl"))
+                            credentials {
+                                username = requireProperty("localRepoUser")
+                                password = requireProperty("localRepoPassword")
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+fun requireProperty(property: String, orElse: String? = null): String =
+    requireNotNull(project.findProperty(property) as? String ?: orElse) {
+        "Could not find property '$property'. Please check the gradle command args. It should contain:\n" +
+                "    ./gradlew ... -P$property=<VALUE> ..."
+    }
+
+fun checkProperty(property: String): Boolean = project.hasProperty(property) && project.property(property) == "true"
+
